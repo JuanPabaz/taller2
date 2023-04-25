bdee8516839e24da869429cfd8784fe413109921
JuanPabaz <jpgthf@gmail.com>
2023-04-25 08:10:38 -0500

    validaciones

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -4,4 +4,4 @@ INSERT INTO clientes(Nombre, Apellido, Email, create_at) VALUES ('Sergio', 'Bula
 INSERT INTO productos(Nombre, Descripcion, Unidades, Precio, codigo) VALUES ('Gaseosa', 'Coca-Cola', 10, 5000, 'G1');
 INSERT INTO productos(Nombre, Descripcion, Unidades, Precio, codigo) VALUES ('Chocolatina','Jumbo', 7, 2500, 'D2');
 
-INSERT INTO usuarios(email, Password, Nombre, Apellido) VALUES ('jpgt400@gmail.com', '$2a$10$y/GqqtAxnm3MQoRBLtCVgu2cggyA/Yp.fjgcliKDos/ALlzk3DxeS', 'Juan', 'Pablo');
+INSERT INTO usuarios(email, Password, Nombre, Apellido) VALUES ('alejo123@gmail.com', '$2a$10$y/GqqtAxnm3MQoRBLtCVgu2cggyA/Yp.fjgcliKDos/ALlzk3DxeS', 'Juan', 'Pablo');
